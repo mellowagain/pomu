4ea55c4b6484409b062f21d76aadc8db9df9bc93
Emily Hudson <emily33901@gmail.com>
2022-07-28 15:34:07 +0000

    Refactor out VideoEntry

--- src/lib/Queue.svelte
+++ src/lib/Queue.svelte
@@ -18,6 +18,7 @@
     import type { VideoInfo } from "./video";
     import { Recording } from "carbon-icons-svelte";
     import Countdown from "svelte-countdown/src/Countdown.svelte";
+    import VideoEntry from "./VideoEntry.svelte";
 
     let loading = true;
 
@@ -73,90 +74,7 @@
 </Row>
 
 {#each [...$queue.entries()] as [id, info] (id)}
-    <Tile style="margin: 20px">
-        <Row padding>
-            <Column>
-                <div style="width: 200px">
-                    <ImageLoader src={info.thumbnail} />
-                </div>
-            </Column>
-            <Column>
-                <Link href="https://youtu.be/{info.id}" target="_blank">
-                    <h4>{info.title}</h4>
-                </Link>
-                <br />
-                <h5>
-                    <OutboundLink
-                        href="https://youtube.com/channel/{info.channelId}"
-                    >
-                        {info.channelName}
-                    </OutboundLink>
-                </h5>
-            </Column>
-            <Column>
-                <p>
-                    {#if Date.now() > new Date(info.scheduledStart).getTime()}
-                        Live since
-                    {:else}
-                        Scheduled for
-                    {/if}
-
-                    {new Date(info.scheduledStart).toTimeString()}
-                </p>
-                <br />
-
-                <Countdown
-                    from={dayjs(info.scheduledStart)}
-                    dateFormat="x"
-                    let:remaining
-                >
-                    {#if !remaining.done}
-                        <p>
-                            Starts in
-                            {#if remaining.days > 0}
-                                <span
-                                    >{remaining.days + remaining.months * 30} day{remaining.days ===
-                                    1
-                                        ? ""
-                                        : "s"}</span
-                                >
-                            {/if}
-
-                            {#if remaining.hours > 0}
-                                <span
-                                    >{remaining.hours} hour{remaining.hours ===
-                                    1
-                                        ? ""
-                                        : "s"}</span
-                                >
-                            {/if}
-
-                            <span
-                                >{remaining.minutes} minute{remaining.minute ===
-                                1
-                                    ? ""
-                                    : "s"}</span
-                            >
-                            <span
-                                >{remaining.seconds} second{remaining.seconds ===
-                                1
-                                    ? ""
-                                    : "s"}</span
-                            >
-                        </p>
-                    {:else}
-                        <Tag icon={Recording} type="red">Live</Tag>
-                    {/if}
-                </Countdown>
-
-                <Tooltip triggerText="Submitters">
-                    {#each info.submitters as submitter}
-                        /{submitter}/
-                    {/each}
-                </Tooltip>
-            </Column>
-        </Row>
-    </Tile>
+    <VideoEntry {info} />
 {/each}
 
 {#if $queue.size === 0 && !loading}
--- src/lib/VideoEntry.svelte
+++ src/lib/VideoEntry.svelte
@@ -1,2 +1,100 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import {
+        Column,
+        ImageLoader,
+        Link,
+        OutboundLink,
+        Row,
+        Tag,
+        Tile,
+        Tooltip,
+    } from "carbon-components-svelte";
+    import Countdown from "svelte-countdown/src";
+    import dayjs from "dayjs";
+    import type { VideoInfo } from "./video";
+    import { Recording } from "carbon-icons-svelte";
 
+    export let info: VideoInfo;
+</script>
+
+<Tile style="margin: 20px">
+    <Row padding>
+        <Column>
+            <div style="width: 200px">
+                <ImageLoader src={info.thumbnail} />
+            </div>
+        </Column>
+        <Column>
+            <Link href="https://youtu.be/{info.id}" target="_blank">
+                <h4>{info.title}</h4>
+            </Link>
+            <br />
+            <h5>
+                <OutboundLink
+                    href="https://youtube.com/channel/{info.channelId}"
+                >
+                    {info.channelName}
+                </OutboundLink>
+            </h5>
+        </Column>
+        <Column>
+            <p>
+                {#if Date.now() > new Date(info.scheduledStart).getTime()}
+                    Live since
+                {:else}
+                    Scheduled for
+                {/if}
+
+                {new Date(info.scheduledStart).toTimeString()}
+            </p>
+            <br />
+
+            <Countdown
+                from={dayjs(info.scheduledStart)}
+                dateFormat="x"
+                let:remaining
+            >
+                {#if !remaining.done}
+                    <p>
+                        Starts in
+                        {#if remaining.days > 0}
+                            <span
+                                >{remaining.days + remaining.months * 30} day{remaining.days ===
+                                1
+                                    ? ""
+                                    : "s"}</span
+                            >
+                        {/if}
+
+                        {#if remaining.hours > 0}
+                            <span
+                                >{remaining.hours} hour{remaining.hours === 1
+                                    ? ""
+                                    : "s"}</span
+                            >
+                        {/if}
+
+                        <span
+                            >{remaining.minutes} minute{remaining.minute === 1
+                                ? ""
+                                : "s"}</span
+                        >
+                        <span
+                            >{remaining.seconds} second{remaining.seconds === 1
+                                ? ""
+                                : "s"}</span
+                        >
+                    </p>
+                {:else}
+                    <Tag icon={Recording} type="red">Live</Tag>
+                {/if}
+            </Countdown>
+
+            <Tooltip triggerText="Submitters">
+                {#each info.submitters as submitter}
+                    /{submitter}/
+                {/each}
+            </Tooltip>
+        </Column>
+    </Row>
+</Tile>
