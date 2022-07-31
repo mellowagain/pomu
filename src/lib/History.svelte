a322cff55701f3580a16c0e7e7d2d18f709e179b
Emily Hudson <emily33901@gmail.com>
2022-07-31 16:49:24 +0000

    refactor queue and history to prevent api spam

--- src/lib/History.svelte
+++ src/lib/History.svelte
@@ -1,10 +1,46 @@
 <script lang="ts">
     import { InlineNotification, Loading, Row } from "carbon-components-svelte";
+    import { onDestroy } from "svelte";
 
     import { readable } from "svelte/store";
     import { showNotification } from "./notifications";
-    import { history } from "./video";
+    import type { VideoInfo } from "./video";
     import VideoEntry from "./VideoEntry.svelte";
+
+    onDestroy(() => {
+        clearTimeout(timeout);
+    });
+
+    let timeout: NodeJS.Timeout;
+    let history = readable<Map<string, VideoInfo>>(new Map(), (set) => {
+        const f = async () => {
+            try {
+                let results = await fetch("/api/history").then((r) => r.json());
+                // transform from array which contains id into id -> array map
+                let map = new Map();
+                for (let r of results) {
+                    map.set(r.id, r);
+                }
+
+                set(map);
+            } catch (e) {
+                showNotification({
+                    title: "Failed to get history",
+                    description: e.text,
+                    kind: "error",
+                    timeout: 5000,
+                });
+            }
+            if (document.hidden) {
+                console.debug("Page is hidden, next update in 30 seconds");
+                timeout = setTimeout(f, 30000);
+            } else {
+                console.debug("Page is visible, next update in 10 seconds");
+                timeout = setTimeout(f, 10000);
+            }
+        };
+        f();
+    });
 
     let loading = true;
     history.subscribe((_) => (loading = false));
--- src/lib/Queue.svelte
+++ src/lib/Queue.svelte
@@ -1,10 +1,47 @@
 <script lang="ts">
     import { InlineNotification, Loading, Row } from "carbon-components-svelte";
+    import { onDestroy } from "svelte";
 
     import { readable } from "svelte/store";
     import { showNotification } from "./notifications";
-    import { queue } from "./video";
+    import type { VideoInfo } from "./video";
     import VideoEntry from "./VideoEntry.svelte";
+
+    onDestroy(() => {
+        clearTimeout(timeout);
+    });
+
+    let timeout: NodeJS.Timeout;
+    let queue = readable<Map<string, VideoInfo>>(new Map(), (set) => {
+        const f = async () => {
+            try {
+                let results = await fetch("/api/queue").then((r) => r.json());
+                // transform from array which contains id into id -> array map
+                let map = new Map();
+                for (let r of results) {
+                    map.set(r.id, r);
+                }
+
+                set(map);
+            } catch (e) {
+                showNotification({
+                    title: "Failed to get queue",
+                    description: e.text,
+                    kind: "error",
+                    timeout: 5000,
+                });
+            }
+
+            if (document.hidden) {
+                console.debug("Page is hidden, next update in 30 seconds");
+                timeout = setTimeout(f, 30000);
+            } else {
+                console.debug("Page is visible, next update in 10 seconds");
+                timeout = setTimeout(f, 10000);
+            }
+        };
+        f();
+    });
 
     let loading = true;
     queue.subscribe((_) => (loading = false));
